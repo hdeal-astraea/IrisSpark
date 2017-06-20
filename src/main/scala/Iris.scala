1ffaa2d391e990635e194014bfca0b11b7dec3e5
jtnachbar <nachbarjamie@gmail.com>
2017-06-20 10:00:18 -0400

    Decision tree meant to determine iris type

--- src/main/scala/Iris.scala
+++ src/main/scala/Iris.scala
@@ -1,8 +1,9 @@
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorAssembler}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.mllib.tree.DecisionTree
-import org.apache.spark.mllib.tree.model.DecisionTreeModel
-import org.apache.spark.mllib.util.MLUtils
 
 
 object IrisSpark {
@@ -19,13 +20,37 @@ object IrisSpark {
       .option("inferSchema", "true")
       .csv(path)
     data.printSchema()
-    val split = data.randomSplit(Array(.5,.5))
-    val dataOne = split(0)
-    val dataTwo = split(1)
-    dataOne.show()
-    dataTwo.show()
-    //dataOne.groupBy("SepalLength").pivot("SepalLength").mean("SepalLength")
+    val Array(train, test) = data.randomSplit(Array(.8,.2))
+    val featVec = new VectorAssembler()
+      .setInputCols(Array("SepalLength", "SepalWidth", "PetalLength", "PetalWidth"))
+      .setOutputCol("features")
+//    val trainFeatures = assembler.transform(dataOne)
+//    val testFeatures = assembler.transform(dataTwo)
+    //need LabelIndexer
+    val nameIndexer = new StringIndexer()
+        .setInputCol("Name")
+        .setOutputCol("nameIndex")
+        .fit(data)
+    val predConverter = new IndexToString()
+        .setInputCol("prediction")
+        .setOutputCol("predLabel")
+        .setLabels(nameIndexer.labels)
+    val tree = new DecisionTreeClassifier()
+      .setLabelCol("nameIndex")
+      .setFeaturesCol("features")
+    val pipeline = new Pipeline()
+      .setStages(Array(nameIndexer, featVec, tree, predConverter))
+    val model = pipeline.fit(train)
+    val preds = model.transform(test)
+    preds.select("Name", "predLabel").show(10)
+    val evaluator = new MulticlassClassificationEvaluator()
+        .setLabelCol("nameIndex")
+        .setPredictionCol("prediction")
+        .setMetricName("accuracy")
+    val accuracy = evaluator.evaluate(preds)
+    println("accuracy: " + (accuracy * 100) + "%")
     //dataOne.join(dataTwo, dataOne("Name"), "outer")
-    (data.groupBy("Name").mean("SepalWidth")).show()
+    //(data.groupBy("Name").mean("SepalWidth", "SepalLength")).show()
+
   }
 }
